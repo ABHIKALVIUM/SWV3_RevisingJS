e592b50a719bcb3678ba7a7196322f90b0c0cb25
ABHIKALVIUM <144119494+ABHIKALVIUM@users.noreply.github.com>
2025-01-16 14:39:53 +0530

    Created Memory Game

--- Memory Game/script.js
+++ Memory Game/script.js
@@ -10,62 +10,48 @@ let interval;
 let firstCard = false;
 let secondCard = false;
 
-//Items array
 const items = [
   { name: "fries", image: "./images/fries.png" },
-  { name: "crocodile", image: "./images/cheeseburger.png" },
-  { name: "macaw", image: "./images/hotdog.png" },
-  { name: "gorilla", image: "./images/ice-cream.png" },
-  { name: "tiger", image: "./images/milkshake.png" },
-  { name: "monkey", image: "./images/pizza.png" },
-//   { name: "chameleon", image: "./images/white.png" },
-  { name: "piranha", image: "./images/sandwich.png" },
-  { name: "anaconda", image: "./images/cupcake.png" },
-  { name: "sloth", image: "./images/chips.png" },
-  { name: "cockatoo", image: "./images/cake.png" },
-  { name: "toucan", image: "./images/burger.png" },
+  { name: "cheeseburger", image: "./images/cheeseburger.png" },
+  { name: "hotdog", image: "./images/hotdog.png" },
+  { name: "ice-cream", image: "./images/ice-cream.png" },
+  { name: "milkshake", image: "./images/milkshake.png" },
+  { name: "pizza", image: "./images/pizza.png" },
+  { name: "sandwich", image: "./images/sandwich.png" },
+  { name: "cupcake", image: "./images/cupcake.png" },
+  { name: "chips", image: "./images/chips.png" },
+  { name: "cake", image: "./images/cake.png" },
+  { name: "burger", image: "./images/burger.png" },
 ];
 
-//Initial Time
 let seconds = 0,
   minutes = 0;
-//Initial moves and win count
 let movesCount = 0,
   winCount = 0;
 
-//For timer
 const timeGenerator = () => {
   seconds += 1;
-  //minutes logic
   if (seconds >= 60) {
     minutes += 1;
     seconds = 0;
   }
-  //format time before displaying
   let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
   let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
   timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
 };
 
-//For calculating moves
 const movesCounter = () => {
   movesCount += 1;
   moves.innerHTML = `<span>Moves:</span>${movesCount}`;
 };
 
-//Pick random objects from the items array
 const generateRandom = (size = 4) => {
-  //temporary array
   let tempArray = [...items];
-  //initializes cardValues array
   let cardValues = [];
-  //size should be double (4*4 matrix)/2 since pairs of objects would exist
   size = (size * size) / 2;
-  //Random object selection
   for (let i = 0; i < size; i++) {
     const randomIndex = Math.floor(Math.random() * tempArray.length);
     cardValues.push(tempArray[randomIndex]);
-    //once selected remove the object from temp array
     tempArray.splice(randomIndex, 1);
   }
   return cardValues;
@@ -74,15 +60,8 @@ const generateRandom = (size = 4) => {
 const matrixGenerator = (cardValues, size = 4) => {
   gameContainer.innerHTML = "";
   cardValues = [...cardValues, ...cardValues];
-  //simple shuffle
   cardValues.sort(() => Math.random() - 0.5);
   for (let i = 0; i < size * size; i++) {
-    /*
-        Create Cards
-        before => front side (contains question mark)
-        after => back side (contains actual image);
-        data-card-values is a custom attribute which stores the names of the cards to match later
-      */
     gameContainer.innerHTML += `
   <div class="card-container" data-card-value="${cardValues[i].name}">
     <div class="card-before">
@@ -95,46 +74,31 @@ const matrixGenerator = (cardValues, size = 4) => {
 `;
 
   }
-  //Grid
   gameContainer.style.gridTemplateColumns = `repeat(${size},auto)`;
 
-  //Cards
   cards = document.querySelectorAll(".card-container");
   cards.forEach((card) => {
     card.addEventListener("click", () => {
-      //If selected card is not matched yet then only run (i.e already matched card when clicked would be ignored)
       if (!card.classList.contains("matched")) {
-        //flip the cliked card
         card.classList.add("flipped");
-        //if it is the firstcard (!firstCard since firstCard is initially false)
         if (!firstCard) {
-          //so current card will become firstCard
           firstCard = card;
-          //current cards value becomes firstCardValue
           firstCardValue = card.getAttribute("data-card-value");
         } else {
-          //increment moves since user selected second card
           movesCounter();
-          //secondCard and value
           secondCard = card;
           let secondCardValue = card.getAttribute("data-card-value");
           if (firstCardValue == secondCardValue) {
-            //if both cards match add matched class so these cards would beignored next time
             firstCard.classList.add("matched");
             secondCard.classList.add("matched");
-            //set firstCard to false since next card would be first now
             firstCard = false;
-            //winCount increment as user found a correct match
             winCount += 1;
-            //check if winCount ==half of cardValues
             if (winCount == Math.floor(cardValues.length / 2)) {
               result.innerHTML = `<h2>You Won</h2>
             <h4>Moves: ${movesCount}</h4>`;
               stopGame();
             }
           } else {
-            //if the cards dont match
-            //flip the cards back to normal
             let [tempFirst, tempSecond] = [firstCard, secondCard];
             firstCard = false;
             secondCard = false;
@@ -149,23 +113,18 @@ const matrixGenerator = (cardValues, size = 4) => {
   });
 };
 
-//Start game
 startButton.addEventListener("click", () => {
   movesCount = 0;
   seconds = 0;
   minutes = 0;
-  //controls amd buttons visibility
   controls.classList.add("hide");
   stopButton.classList.remove("hide");
   startButton.classList.add("hide");
-  //Start timer
   interval = setInterval(timeGenerator, 1000);
-  //initial moves
   moves.innerHTML = `<span>Moves:</span> ${movesCount}`;
   initializer();
 });
 
-//Stop game
 stopButton.addEventListener(
   "click",
   (stopGame = () => {
@@ -176,7 +135,6 @@ stopButton.addEventListener(
   })
 );
 
-//Initialize values and func calls
 const initializer = () => {
   result.innerText = "";
   winCount = 0;
